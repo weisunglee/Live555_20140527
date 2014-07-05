5394c904195ebc96fe8fe4378ff665c83e193f87
Vincent <Vincent.Lee@isapsolution.com>
2014-07-05 12:41:08 +0800

    init repo.

---
+++ README.md
@@ -0,0 +1,8 @@
+Live555_20140704
+================
+
+Live555 with Visual studio
+
+1. Confirm your Visual studio install path in 'win32config' and 'build.bat'.
+2. click genWindowsMakefiles.cmd
+3. click build.bat
---
+++ build.bat
@@ -0,0 +1,15 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+cd liveMedia
+nmake /B -f liveMedia.mak
+cd ..\groupsock
+nmake /B -f groupsock.mak
+cd ..\UsageEnvironment
+nmake /B -f UsageEnvironment.mak
+cd ..\BasicUsageEnvironment
+nmake /B -f BasicUsageEnvironment.mak
+cd ..\testProgs
+nmake /B -f testProgs.mak
+cd ..\mediaServer
+nmake /B -f mediaServer.mak
+cd  ..\proxyServer
+nmake /B -f proxyServer.mak
